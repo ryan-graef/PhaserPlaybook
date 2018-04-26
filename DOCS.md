cbb7575037d736a00b9b2367258f5b823007b83d
Ryan Graef <cudascubby@gmail.com>
2018-04-25 20:58:46 -0500

    creating and moving actors

--- DOCS.md
+++ DOCS.md
@@ -1,21 +1,64 @@
 # PhaserPlaybook Documentation
 
-### Actors
+## Actors
 
 Actors are the key element of your scene.  These will be displayed on the screen and manipulated by the commands you specify.  Your playbook JSON will need an object that looks like this: 
 
 ```json
-    "actors": [
+    "actors": {
+        "myActor": {
+            "sprite_key": "actor-guy"
+        }
+    },
+```
+
+The name of the JSON object (in this case `myActor`) is your actor_id, for reference later. The options for each actor are as follows:
+
+* `sprite_key`: REQUIRED.  The key of the sprite or image you want to render for the actor
+
+
+
+## Directions
+
+Directions are the lifeblood of your scene.  These describe the actions you want to perform with your actors.  Here's how the directions json look:
+
+```json
+    "directions": [
         {
-            "sprite_key": "mySprite",
             "actor_id": "myActor",
-            "initial_position": {"x": 50, "y": 50}
+            "type": "create_actor",
+            "parameters": {
+                "initial_position": {"x": 50, "y": 50},
+                "initial_frame": 0
+            }
         }
     ]
 ```
 
-The options are as follows:
+The important things every direction needs are:
 
-* `sprite_key`: REQUIRED.  The key of the sprite or image you want to render for the actor
-* `actor_id`: REQUIRED.  The key for the actor in the scene.  This will be referenced later.
-* `initial_position`: OPTIONAL.  If you would desire the actor to begin somewhere on the screen, you can use this.  If not present, the actor will begin offscreen.
+* `actor_id`: REQUIRED.  This corresponds with the id of your actor in the `actors` JSON object.
+* `type`: REQUIRED.  This indicates what direction you're performing.  More info on possible values below
+* `parameters`: REQUIRED.  Parameters that will be used to execute the direction.
+
+
+### Direction Types
+
+#### create_actor
+
+Actor sprites are not created initially.  You should call this if you want to create your actor with specific parameters.  If not, the first time the actor is used, this direction will be invoked internally with default parameters.
+
+Parameters:
+
+* `initial_position`: optional.  An object with `x` and `y` properties defining where you want your actor to spawn in.  By default, this is `x: -500, y: -500` (offscreen)
+* `initial_frame`: optional. for animated sprites, the initial frame you want to show.  Defaults to 0.
+
+
+#### move_actor
+
+Performs a simple tween movement on the actor.
+
+Parameters:
+
+* `new_position`: REQUIRED.  An object with `x` and `y` properties defining where you want to end up.
+* `duration`: optional.  Given in ms.  Defaults to 1000.
--- src/playbook.js
+++ src/playbook.js
@@ -3,6 +3,7 @@
 **/
 PhaserPlaybook = function(scene) {
     this.scene = scene;
+    this._plays = [];
 
     /** 
       * Called by Phaser during the sys.install call.  Installs all necessary objects onto Phaser for use in the scene.
@@ -15,12 +16,81 @@ PhaserPlaybook = function(scene) {
       * Enable this scene to run a playbook
       * playbookJsonId {string} - the id of the playbook json in the cache
     **/
-    this.setPlaybook = function(playbookJsonId){
-        var myPlaybook = this.scene.cache.json.get(playbookJsonId);
+    this.addPlaybook = function(playbookJsonId){
+        var playbookObj = this.scene.cache.json.get(playbookJsonId);
 
-        if(!myPlaybook){
+        if(!playbookObj){
             throw "No playbook with id " + playbookJsonId;
         }
+
+        var myPlaybook = {
+            playbookObj: playbookObj,
+            currentDirectionIndex: 0,
+            paused: false
+        }
+
+        this._plays.push(myPlaybook);
+        this._runPlaybook(myPlaybook);
+
+        return myPlaybook;
+    }
+
+    /**
+      * PRIVATE
+    **/
+    this._runPlaybook = function(playbook){
+        while(playbook.currentDirectionIndex < playbook.playbookObj.directions.length){
+            var nextDirection = playbook.playbookObj.directions[playbook.currentDirectionIndex];
+            var nextActor = playbook.playbookObj.actors[nextDirection.actor_id];
+
+            console.log(nextDirection, nextActor);
+            this._executePlaybookDirection(nextActor, nextDirection, playbook);
+
+            playbook.currentDirectionIndex++;
+        }
+    }
+
+    /**
+      * PRIVATE
+    **/
+    this._executePlaybookDirection = function(actor, direction, playbook){
+        switch(direction.type){
+            case 'create_actor':
+                this._createActor(actor, direction.parameters, playbook);
+                break;
+            case 'move_actor':
+                this._moveActor(actor, direction.parameters, playbook);
+                break;
+        }
+    }
+
+    /**
+      * PRIVATE
+    **/
+    this._createActor = function(actor, parameters, playbook){
+        var initPos = parameters.initial_position || {x: -500, y: -500};
+        var initFrame = parameters.initial_frame || 0;
+        
+        actor.sprite = this.scene.add.sprite(initPos.x, initPos.y, actor.sprite_key, initFrame);
+    }
+
+    /**
+      *
+    **/
+    this._moveActor = function(actor, parameters, playbook){
+        var newPos = parameters.new_position;
+        var duration = parameters.duration || 1000;
+
+        if(!actor.sprite){
+            this._createActor(actor, { }, playbook);
+        }
+
+        this.scene.add.tween({
+            targets: actor.sprite,
+            x: newPos.x,
+            y: newPos.x,
+            duration: duration
+        });
     }
 }
 
